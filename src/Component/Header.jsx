4d0e844b718e3b87732caf9dc87de5b2ea106a91
manojglitch <manojkamlu03@gmail.com>
2025-12-17 14:43:08 +0530

    updated header

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import './Header.css'
 
 const Header = () => {
   return (
-    <div className='w-2xl h-1 border-2 bg-emerald-500'>
-      <Link to='/'>Home</Link>
-      <Link to='/Menu'>Menu</Link>
-      <Link to='/Food'>Food</Link>
-      <Link to='/Contact'>Contact</Link>
-    </div>
+    <div className='bg-amber-500'>Header</div>
   )
 }
 
